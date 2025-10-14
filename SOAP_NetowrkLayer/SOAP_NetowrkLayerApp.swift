1898bf900d5f05015a3932b9e0c8699dc15182c3
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2025-10-14 10:01:49 +0300

    Initial Commit

---
+++ SOAP_NetowrkLayer/SOAP_NetowrkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOAP_NetowrkLayerApp.swift
+//  SOAP_NetowrkLayer
+//
+//  Created by iSlam AbdelAziz on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SOAP_NetowrkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
